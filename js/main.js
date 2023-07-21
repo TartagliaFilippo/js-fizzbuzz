5449b70c661eb5af2c716d0a8bcb2c73df8e8299
filippo199600 <pippotartaglia@gmail.com>
2023-07-21 16:01:33 +0200

    Feat target-container

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 //INSERISCO IL CICLO FOR
 for (let i = 1; i <= 100; i++) {
   //console.log(i);
-
+  let numbers = i;
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+    numbers = "FizzBuzz";
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    numbers = "Fizz";
   } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+    numbers = "Buzz";
   }
+  console.log(numbers);
 }
