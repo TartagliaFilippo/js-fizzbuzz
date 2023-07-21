f4a83a3df7a273003547fcde7de5e9234bc558dd
filippo199600 <pippotartaglia@gmail.com>
2023-07-21 16:04:39 +0200

    js connected div container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+const tartgetContainer = document.getElementById("target-container");
+
 //INSERISCO IL CICLO FOR
 for (let i = 1; i <= 100; i++) {
   //console.log(i);
+
+  //VARIABILE DI INDENTIFICAZIONE
   let numbers = i;
+
+  //CONDIZIONI
   if (i % 3 == 0 && i % 5 == 0) {
     numbers = "FizzBuzz";
   } else if (i % 3 == 0) {
